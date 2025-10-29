0619eaddfea0bcab7b98b95540e10f6fa100a941
Menelisi Mthembu <menelismthembu@gmail.com>
2025-10-30 01:45:09 +0200

    Refactor

--- src/main/java/co/appointment/config/OpenApiConfig.java
+++ src/main/java/co/appointment/config/OpenApiConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
+
     @Bean
     public OpenAPI customOpenAPI(final AppConfigProperties appConfigProperties) {
         OpenApiSettings.OpenApiInfo info = appConfigProperties.getOpenApi().getInfo();
