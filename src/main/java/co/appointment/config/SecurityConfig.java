a60d8e0dc8840dcef2dda2af7b0df04d09e4b3a8
Menelisi Mthembu <menelismthembu@gmail.com>
2025-11-12 01:31:40 +0200

    Added auth platform

--- src/main/java/co/appointment/config/SecurityConfig.java
+++ src/main/java/co/appointment/config/SecurityConfig.java
@@ -1,47 +1,54 @@
 package co.appointment.config;
 
-import co.appointment.service.CustomAccessDeniedHandler;
-import co.appointment.service.CustomAuthEntryPoint;
-import co.appointment.service.JwtReactiveAuthenticationManager;
-import co.appointment.service.JwtServerAuthenticationConverter;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
-import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
 
 @Configuration
 @EnableWebFluxSecurity
 @Slf4j
 public class SecurityConfig {
 
-    @Bean
-    public SecurityWebFilterChain securityWebFilterChain(final ServerHttpSecurity http,
-                                                         final AuthenticationWebFilter jwtAuthenticationWebFilter,
-                                                         final AppConfigProperties appConfigProperties,
-                                                         final ObjectMapper objectMapper) {
-        return http
-                .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .exceptionHandling(exception -> exception.accessDeniedHandler(new CustomAccessDeniedHandler(objectMapper))
-                        .authenticationEntryPoint(new CustomAuthEntryPoint(objectMapper)))
-                .authorizeExchange(exchanges -> exchanges
-                        .pathMatchers(appConfigProperties.getWhiteList()).permitAll()
-                        .anyExchange().authenticated()
-                )
-                .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
-                .addFilterAt(jwtAuthenticationWebFilter, SecurityWebFiltersOrder.AUTHENTICATION)
-                .build();
-    }
-    @Bean
-    public AuthenticationWebFilter jwtAuthenticationWebFilter(final JwtReactiveAuthenticationManager authManager,
-                                                              final JwtServerAuthenticationConverter converter) {
-        AuthenticationWebFilter filter = new AuthenticationWebFilter(authManager);
-        filter.setServerAuthenticationConverter(converter);
-        return filter;
-    }
+    //TODO: Handle multiple identity providers
+//    @Value("${spring.security.oauth2.client.provider.spring.issuer-uri}")
+//    private String issuerUri;
+//
+//    @Bean
+//    public SecurityWebFilterChain springSecurityFilterChain(final ServerHttpSecurity http) {
+//        return http
+//                .authorizeExchange(auth -> auth.anyExchange().authenticated())
+//                .oauth2Login(Customizer.withDefaults())
+//                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
+//                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+//                .build();
+//    }
+//    @Bean
+//    public ReactiveJwtDecoder jwtDecoder() {
+//        return ReactiveJwtDecoders.fromIssuerLocation(issuerUri);
+//    }
+
+//    @Bean
+//    public SecurityWebFilterChain securityWebFilterChain(final ServerHttpSecurity http,
+//                                                         final AuthenticationWebFilter jwtAuthenticationWebFilter,
+//                                                         final AppConfigProperties appConfigProperties,
+//                                                         final ObjectMapper objectMapper) {
+//        return http
+//                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+//                .exceptionHandling(exception -> exception.accessDeniedHandler(new CustomAccessDeniedHandler(objectMapper))
+//                        .authenticationEntryPoint(new CustomAuthEntryPoint(objectMapper)))
+//                .authorizeExchange(exchanges -> exchanges
+//                        .pathMatchers(appConfigProperties.getWhiteList()).permitAll()
+//                        .anyExchange().authenticated()
+//                )
+//                .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
+//                .addFilterAt(jwtAuthenticationWebFilter, SecurityWebFiltersOrder.AUTHENTICATION)
+//                .build();
+//    }
+//    @Bean
+//    public AuthenticationWebFilter jwtAuthenticationWebFilter(final JwtReactiveAuthenticationManager authManager,
+//                                                              final JwtServerAuthenticationConverter converter) {
+//        AuthenticationWebFilter filter = new AuthenticationWebFilter(authManager);
+//        filter.setServerAuthenticationConverter(converter);
+//        return filter;
+//    }
 }
