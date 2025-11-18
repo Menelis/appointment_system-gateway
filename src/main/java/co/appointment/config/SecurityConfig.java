d120f0b649a5ad42e6d1e92d472e66c7a3b17a99
Menelisi Mthembu <menelismthembu@gmail.com>
2025-11-18 02:06:24 +0200

    Refactor

--- src/main/java/co/appointment/config/SecurityConfig.java
+++ src/main/java/co/appointment/config/SecurityConfig.java
@@ -1,35 +1,37 @@
 package co.appointment.config;
 
-import co.appointment.shared.model.CorsSettings;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
 
+@Profile("!test")
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    private String jwkSetUri;
+
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(final ServerHttpSecurity http,
                                                             final AppConfigProperties appConfigProperties) throws Exception {
-        CorsSettings corsSettings = appConfigProperties.getCors();
         return http
-                .cors(cors -> cors.configurationSource(_ -> {
-                    CorsConfiguration configuration = new CorsConfiguration();
-                    configuration.setAllowedOrigins(corsSettings.getAllowedOrigins());
-                    configuration.setAllowedMethods(corsSettings.getAllowedMethods());
-                    configuration.setAllowedHeaders(corsSettings.getAllowedHeaders());
-                    return configuration;
-                }))
                 .authorizeExchange(auth -> auth.anyExchange().authenticated())
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .build();
+    }
+
+    @Bean
+    public ReactiveJwtDecoder jwtDecoder() {
+        return NimbusReactiveJwtDecoder.withJwkSetUri(jwkSetUri)
                 .build();
     }
 }
